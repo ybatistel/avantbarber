871adb2a5fe8eebcfc1775089d49a6367fc5f541
ybatistel <yannbatistell@gmail.com>
2026-07-08 15:41:34 -0300

    Testando ci

--- avant/src/main/java/com/avantbarber/avant/AvantApplication.java
+++ avant/src/main/java/com/avantbarber/avant/AvantApplication.java
@@ -2,8 +2,10 @@ package com.avantbarber.avant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class AvantApplication {
 
 	public static void main(String[] args) {
