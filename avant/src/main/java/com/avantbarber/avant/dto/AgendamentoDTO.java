8aa3825e6187a5283afceda88a617939dec28417
ybatistel <yannbatistell@gmail.com>
2026-07-24 16:30:39 -0300

    feat(service): novas validações para AgendamentoService

--- avant/src/main/java/com/avantbarber/avant/dto/AgendamentoDTO.java
+++ avant/src/main/java/com/avantbarber/avant/dto/AgendamentoDTO.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import com.avantbarber.avant.model.Agendamento;
+import com.avantbarber.avant.model.StatusAgendamento;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,8 +23,19 @@ public class AgendamentoDTO {
     private BigDecimal precoServico;
     private String cpfCliente;
     private LocalDateTime dataHora;
-    private String status;
+    private StatusAgendamento status;
 
     public AgendamentoDTO(Agendamento agendamento) {
+        this.id = agendamento.getId();
+        this.clienteId = agendamento.getCliente().getId();
+        this.servicoId = agendamento.getServico().getId();
+        this.barbeiroId = agendamento.getBarbeiro().getId();
+        this.nomeBarbeiro = agendamento.getBarbeiro().getNome();
+        this.nomeCliente = agendamento.getCliente().getNome();
+        this.nomeServico = agendamento.getServico().getNome();
+        this.precoServico = agendamento.getServico().getPreco();
+        this.cpfCliente = agendamento.getCliente().getCpf();
+        this.dataHora = agendamento.getData();
+        this.status = agendamento.getStatus();
     }
 }
