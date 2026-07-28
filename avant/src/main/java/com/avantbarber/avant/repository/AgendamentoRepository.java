9f893bdc7ff083f98270d8158b8d15d1a50451f3
ybatistel <yannbatistell@gmail.com>
2026-07-28 15:29:02 -0300

    feat: adiciona calculo de horarios disponiveis no AgendamentoService

--- avant/src/main/java/com/avantbarber/avant/repository/AgendamentoRepository.java
+++ avant/src/main/java/com/avantbarber/avant/repository/AgendamentoRepository.java
@@ -1,13 +1,20 @@
 package com.avantbarber.avant.repository;
 
 import com.avantbarber.avant.model.Agendamento;
+import com.avantbarber.avant.model.StatusAgendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     Optional<Agendamento> findById (Long id);
+    List<Agendamento> findByBarbeiroIdAndDataBetweenAndStatusNot(
+            Long barbeiroId,
+            LocalDateTime dataInicio,
+            LocalDateTime dataFim,
+            StatusAgendamento statusAgendamento);
 
     boolean existsByBarbeiroIdAndData(Long barbeiroId, LocalDateTime data);
     boolean existsByClienteIdAndData(Long clienteId, LocalDateTime data);
